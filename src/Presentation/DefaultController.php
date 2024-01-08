152f30f478b1921a999f86eac8e90297b312b534
Julien Mercier-Rojas <julien@jeckel-lab.fr>
2024-01-08 11:24:31 +0100

    Initialize Dashboard

---
+++ src/Presentation/DefaultController.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * @author: Julien Mercier-Rojas <julien@jeckel-lab.fr>
+ * Created at: 08/01/2024
+ */
+
+declare(strict_types=1);
+
+namespace App\Presentation;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class DefaultController extends AbstractController
+{
+    #[Route('/')]
+    public function index(): Response
+    {
+        return $this->redirectToRoute('admin');
+    }
+}
