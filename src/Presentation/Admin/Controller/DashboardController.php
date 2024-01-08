5ceaa81a48eb24ae92a08989066d243c7c193615
Julien Mercier-Rojas <julien@jeckel-lab.fr>
2024-01-08 11:29:33 +0100

    Force php8.3

--- src/Presentation/Admin/Controller/DashboardController.php
+++ src/Presentation/Admin/Controller/DashboardController.php
@@ -13,23 +13,7 @@ class DashboardController extends AbstractDashboardController
     #[Route('/admin', name: 'admin')]
     public function index(): Response
     {
-//        return parent::index();
-
-        // Option 1. You can make your dashboard redirect to some common page of your backend
-        //
-        // $adminUrlGenerator = $this->container->get(AdminUrlGenerator::class);
-        // return $this->redirect($adminUrlGenerator->setController(OneOfYourCrudController::class)->generateUrl());
-
-        // Option 2. You can make your dashboard redirect to different pages depending on the user
-        //
-        // if ('jane' === $this->getUser()->getUsername()) {
-        //     return $this->redirect('...');
-        // }
-
-        // Option 3. You can render some custom template to display a proper dashboard with widgets, etc.
-        // (tip: it's easier if your template extends from @EasyAdmin/page/content.html.twig)
-        //
-         return $this->render('admin/dashboard.html.twig');
+        return $this->render('admin/dashboard.html.twig');
     }
 
     public function configureDashboard(): Dashboard
@@ -39,7 +23,7 @@ class DashboardController extends AbstractDashboardController
             ->setTranslationDomain('admin')
             ->renderContentMaximized()
             ->setLocales(['fr'])
-            ;
+        ;
     }
 
     public function configureMenuItems(): iterable
