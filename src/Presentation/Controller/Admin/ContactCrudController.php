1a73eaa08c48581d25074f3f71e1295afaec9f98
Julien Mercier-Rojas <julien@jeckel-lab.fr>
2024-02-07 12:34:34 +0100

    List mails on contact page

--- src/Presentation/Controller/Admin/ContactCrudController.php
+++ src/Presentation/Controller/Admin/ContactCrudController.php
@@ -10,10 +10,19 @@ declare(strict_types=1);
 namespace App\Presentation\Controller\Admin;
 
 use App\Infrastructure\Doctrine\Entity\Contact;
+use App\Presentation\Field\ControllerIndexField;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Contracts\Field\FieldInterface;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\CollectionField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\EmailField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\FormField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TelephoneField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 class ContactCrudController extends AbstractCrudController
 {
@@ -40,8 +49,36 @@ class ContactCrudController extends AbstractCrudController
             ->add(Crud::PAGE_INDEX, $syncVCard)
             ->update(Crud::PAGE_INDEX, Action::DETAIL, static function (Action $action) {
                 return $action->setIcon('fa fa-eye')
-//                    ->setLabel(false)
                     ->setCssClass('btn btn-secondary');
             });
+    }
+
+    /**
+     * @param string $pageName
+     * @return iterable<FieldInterface>
+     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
+     */
+    public function configureFields(string $pageName): iterable
+    {
+        if ($pageName === Crud::PAGE_INDEX) {
+            yield TextField::new('displayName');
+            yield EmailField::new('email');
+            yield TelephoneField::new('phoneNumber');
+            yield AssociationField::new('mails')
+                ->onlyOnIndex();
+        }
+        if ($pageName === Crud::PAGE_DETAIL) {
+            yield FormField::addTab('sommaire');
+            yield TextField::new('displayName');
+            yield EmailField::new('email');
+            yield TextField::new('firstName');
+            yield TextField::new('lastName');
+            yield TelephoneField::new('phoneNumber');
+
+            yield FormField::addTab('Mails');
+            yield AssociationField::new('mails')
+                ->setSortProperty('date')
+                ->setTemplatePath('admin/field/contact_mails.html.twig');
+        }
     }
 }
