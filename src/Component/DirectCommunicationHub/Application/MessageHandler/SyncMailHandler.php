f8d515fc252006a61775009f78ea2d378de18a50
Julien Mercier-Rojas <julien@jeckel-lab.fr>
2024-02-15 23:15:37 +0100

    Fix QA

--- src/Component/DirectCommunicationHub/Application/MessageHandler/SyncMailHandler.php
+++ src/Component/DirectCommunicationHub/Application/MessageHandler/SyncMailHandler.php
@@ -46,7 +46,7 @@ readonly class SyncMailHandler
         $this->mailRegisterer->register(new IncomingMailDto(
             id: $syncMail->mailId,
             messageId: $mail->messageId ?? '',
-//            messageId: $mail->messageId ?? throw new LogicException('Message Id can not be null'),
+            //            messageId: $mail->messageId ?? throw new LogicException('Message Id can not be null'),
             date: new DateTimeImmutable($mail->date ?? throw new LogicException('Date can not be null')),
             subject: $mail->subject ?? throw new LogicException('Subject can not be null'),
             fromName: $mail->fromName ?? $mail->fromAddress ?? throw new LogicException('Both fromName and fromAddress can not be null'),
