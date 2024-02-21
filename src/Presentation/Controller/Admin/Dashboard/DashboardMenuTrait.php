8d3eabcf95befa4af28aebeaba795b02b9c2f1ad
Julien Mercier-Rojas <julien@jeckel-lab.fr>
2024-02-21 09:09:07 +0100

    Remove account filter on dashboard menu

--- src/Presentation/Controller/Admin/Dashboard/DashboardMenuTrait.php
+++ src/Presentation/Controller/Admin/Dashboard/DashboardMenuTrait.php
@@ -18,8 +18,6 @@ use App\Infrastructure\Doctrine\Entity\Contact;
 use App\Infrastructure\Doctrine\Entity\ImapConfig;
 use App\Infrastructure\Doctrine\Entity\IncomingWebhook;
 use App\Infrastructure\Doctrine\Entity\Mail;
-use App\Infrastructure\Doctrine\Entity\User;
-use EasyCorp\Bundle\EasyAdminBundle\Config\Menu\CrudMenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 
 trait DashboardMenuTrait
@@ -27,27 +25,15 @@ trait DashboardMenuTrait
     public function configureMenuItems(): iterable
     {
         yield MenuItem::linkToDashboard('menu.dashboard', 'fa fa-home');
-        yield $this->filterByAccount(
-            MenuItem::linkToCrud('menu.contacts', 'fas fa-id-card', Contact::class)
-        );
-        yield $this->filterByAccount(
-            MenuItem::linkToCrud('menu.companies', 'fa fa-building', Company::class)
-        );
-        yield $this->filterByAccount(
-            MenuItem::linkToCrud('menu.mail', 'fa fa-inbox', Mail::class)
-        );
+        yield MenuItem::linkToCrud('menu.contacts', 'fas fa-id-card', Contact::class);
+        yield MenuItem::linkToCrud('menu.companies', 'fa fa-building', Company::class);
+        yield MenuItem::linkToCrud('menu.mail', 'fa fa-inbox', Mail::class);
         yield MenuItem::section('menu.admin');
         yield MenuItem::subMenu('menu.config', 'fa fa-wrench')
             ->setSubItems([
-                $this->filterByAccount(
-                    MenuItem::linkToCrud('menu.card_dav', 'fas fa-id-card', CardDavConfig::class)
-                ),
-                $this->filterByAccount(
-                    MenuItem::linkToCrud('menu.imap', 'fa fa-inbox', ImapConfig::class)
-                ),
-                $this->filterByAccount(
-                    MenuItem::linkToCrud('menu.services', 'fas fa-concierge-bell', AccountService::class)
-                ),
+                MenuItem::linkToCrud('menu.card_dav', 'fas fa-id-card', CardDavConfig::class),
+                MenuItem::linkToCrud('menu.imap', 'fa fa-inbox', ImapConfig::class),
+                MenuItem::linkToCrud('menu.services', 'fas fa-concierge-bell', AccountService::class),
             ])
             ->setPermission('ROLE_ADMIN');
         yield MenuItem::subMenu('menu.super_admin', 'fa fa-wrench')
@@ -58,16 +44,5 @@ trait DashboardMenuTrait
                 MenuItem::linkToCrud('menu.accounts', 'fas fa-wrench', Account::class),
             ])
             ->setPermission('ROLE_SUPER_ADMIN');
-    }
-
-    protected function filterByAccount(CrudMenuItem $menuItem): CrudMenuItem
-    {
-        /** @var User $user */
-        $user = $this->getUser();
-        if ($user->hasRole('ROLE_SUPER_ADMIN')) {
-            $menuItem->setQueryParameter('filters[account][comparison]', '=')
-                ->setQueryParameter('filters[account][value]', $user->getAccountOrFail()->getId());
-        }
-        return $menuItem;
     }
 }
