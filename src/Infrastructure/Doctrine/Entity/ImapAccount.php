8ed3796d6944d4dec090a7907c6d42156054d36e
Julien Mercier-Rojas <julien@jeckel-lab.fr>
2024-02-26 10:04:34 +0100

    Refactor IMAP entities

--- src/Infrastructure/Doctrine/Entity/ImapAccount.php
+++ src/Infrastructure/Doctrine/Entity/ImapAccount.php
@@ -9,19 +9,20 @@ declare(strict_types=1);
 
 namespace App\Infrastructure\Doctrine\Entity;
 
-use App\Infrastructure\Doctrine\Repository\ImapConfigRepository;
+use App\Infrastructure\Doctrine\Repository\ImapAccountRepository;
 use Doctrine\ORM\Mapping as ORM;
 use Ramsey\Uuid\UuidInterface;
+use SensitiveParameter;
 use Stringable;
 
 /**
  * @SuppressWarnings(PHPMD.UnusedPrivateField)
  */
-#[ORM\Entity(repositoryClass: ImapConfigRepository::class)]
-class ImapConfig implements Stringable
+#[ORM\Entity(repositoryClass: ImapAccountRepository::class)]
+class ImapAccount implements Stringable
 {
     #[ORM\Id]
-    #[ORM\Column(name: 'imap_config_id', type: "uuid", unique: true)]
+    #[ORM\Column(name: 'imap_account_id', type: "uuid", unique: true)]
     #[ORM\GeneratedValue(strategy: "NONE")]
     private UuidInterface|string $id;
 
@@ -48,7 +49,7 @@ class ImapConfig implements Stringable
         return $this->id;
     }
 
-    public function setId(UuidInterface|string $id): ImapConfig
+    public function setId(UuidInterface|string $id): ImapAccount
     {
         $this->id = $id;
         return $this;
@@ -59,7 +60,7 @@ class ImapConfig implements Stringable
         return $this->name;
     }
 
-    public function setName(string $name): ImapConfig
+    public function setName(string $name): ImapAccount
     {
         $this->name = $name;
         return $this;
@@ -70,7 +71,7 @@ class ImapConfig implements Stringable
         return $this->uri;
     }
 
-    public function setUri(string $uri): ImapConfig
+    public function setUri(string $uri): ImapAccount
     {
         $this->uri = $uri;
         return $this;
@@ -81,7 +82,7 @@ class ImapConfig implements Stringable
         return $this->login;
     }
 
-    public function setLogin(string $login): ImapConfig
+    public function setLogin(string $login): ImapAccount
     {
         $this->login = $login;
         return $this;
@@ -92,7 +93,7 @@ class ImapConfig implements Stringable
         return $this->password;
     }
 
-    public function setPassword(string $password): ImapConfig
+    public function setPassword(#[SensitiveParameter] string $password): ImapAccount
     {
         $this->password = $password;
         return $this;
@@ -110,7 +111,7 @@ class ImapConfig implements Stringable
      * @param array<int, string> $folders
      * @return $this
      */
-    public function setFolders(array $folders): ImapConfig
+    public function setFolders(array $folders): ImapAccount
     {
         $this->folders = $folders;
         return $this;
