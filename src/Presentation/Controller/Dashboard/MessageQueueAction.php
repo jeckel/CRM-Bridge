d90db7b91074993533f01bf952a9875fc7f46126
Julien Mercier-Rojas <julien@jeckel-lab.fr>
2024-02-28 07:58:44 +0100

    Prepare dashboard widgets

--- src/Presentation/Controller/Dashboard/MessageQueueAction.php
+++ src/Presentation/Controller/Dashboard/MessageQueueAction.php
@@ -1,32 +1,29 @@
 <?php
 
 /**
- * @author: Julien Mercier-Rojas <julien@jeckel-lab.fr>
- * Created at: 26/02/2024
+ * @author Julien Mercier-Rojas <julien@jeckel-lab.fr>
+ * Created at: 27/02/2024 08:49
  */
-
 declare(strict_types=1);
 
 namespace App\Presentation\Controller\Dashboard;
 
-use Supervisor\Supervisor;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class WorkerStatusAction extends AbstractController
+class MessageQueueAction extends AbstractController
 {
     #[Route(
-        path: '/dashboard/parts/workers',
-        name: 'dashboard_parts_worker_status',
+        path: '/dashboard/parts/messengers',
+        name: 'dashboard_parts_messengers',
         methods: ['GET']
     )]
     public function index(
-        Supervisor $supervisor
     ): Response {
         return $this->render(
-            'dashboard/worker_status.html.twig',
-            ['workers' => $supervisor->getAllProcesses()]
+            'dashboard/messenger_status.html.twig',
+            []
         );
     }
 }
