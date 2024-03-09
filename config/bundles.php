45a88bb9012450418de3c52753acf96afbc0a6bd
Julien Mercier-Rojas <julien@jeckel-lab.fr>
2024-03-09 21:06:56 +0100

    Implements pagination on mail list

--- config/bundles.php
+++ config/bundles.php
@@ -15,4 +15,5 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     KevinPapst\TablerBundle\TablerBundle::class => ['all' => true],
     Symfony\UX\TwigComponent\TwigComponentBundle::class => ['all' => true],
+    Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
 ];
--- src/Presentation/Controller/WebMail/MailListController.php
+++ src/Presentation/Controller/WebMail/MailListController.php
@@ -10,14 +10,12 @@ declare(strict_types=1);
 namespace App\Presentation\Controller\WebMail;
 
 use App\Infrastructure\Doctrine\Entity\Contact;
-use App\Infrastructure\Doctrine\Entity\ImapMailbox;
 use App\Infrastructure\Doctrine\Entity\ImapMessage;
-use App\Infrastructure\Doctrine\Repository\ImapAccountRepository;
 use App\Infrastructure\Doctrine\Repository\ImapMailboxRepository;
-use App\Infrastructure\Doctrine\Repository\ImapMessageRepository;
-use App\Infrastructure\Imap\ImapMailboxConnector;
 use Doctrine\ORM\EntityManagerInterface;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -30,41 +28,35 @@ class MailListController extends AbstractController
     )]
     public function listMails(
         string $mailboxId,
-        //        ImapMessageRepository $repository
-        //        string $folderPath,
-        //        ImapAccountRepository $repository,
         ImapMailboxRepository $mailboxRepository,
-        EntityManagerInterface $entityManager
+        EntityManagerInterface $entityManager,
+        PaginatorInterface $paginator,
+        Request $request
     ): Response {
-        //        $imapAccount = $repository->getById($imapConfigId);
-        //        $mailbox = ImapMailboxConnector::fromImapAccount($imapAccount);
-        //        $mailIds = $mailbox->searchFolder($folderPath);
-
-        // @todo : move into the repository
         $mailbox = $mailboxRepository->getById($mailboxId);
         $query = $entityManager->createQueryBuilder();
-        $mails = $query->select('m.id, m.subject, m.fromName, m.fromAddress, m.date, m.isTreated, m.spamHeaders, c.id as authorId, c.displayName as authorName')
-            ->from(ImapMessage::class, 'm')
-            ->leftJoin(Contact::class, 'c', 'WITH', 'c.id = m.contact')
-            ->where('m.imapMailbox = :imapMailbox')
-//            ->andWhere('m.folder = :folder')
-//            ->andWhere('m.uid IN (:mailIds)')
-            ->setParameter(':imapMailbox', $mailbox)
-//            ->setParameter(':folder', $folderPath)
-//            ->setParameter(':mailIds', $mailIds)
-            ->orderBy('m.date', 'DESC')
-            ->getQuery()
-            ->execute();
 
+        $page = $request->query->getInt('page', 1);
+        $limit = 20;
+        $mails = $paginator->paginate(
+            $query->select('m.id, m.subject, m.fromName, m.fromAddress, m.date, m.isTreated, m.spamHeaders, c.id as authorId, c.displayName as authorName')
+                ->from(ImapMessage::class, 'm')
+                ->leftJoin(Contact::class, 'c', 'WITH', 'c.id = m.contact')
+                ->where('m.imapMailbox = :imapMailbox')
+                ->setParameter(':imapMailbox', $mailbox)
+                ->orderBy('m.date', 'DESC'),
+            $page,
+            $limit
+        );
 
         return $this->render(
             'webmail/mail_list.html.twig',
             [
-//                'imapConfigId' => $imapConfigId,
-//                'folder' => $folderPath,
-//                'mailIds' => $mailIds,
                 'mailbox' => $mailbox,
                 'mails' => $mails,
+                'page' => $page,
+                'limit' => $limit,
+                'total' => $mails->getTotalItemCount()
             ]
         );
     }
