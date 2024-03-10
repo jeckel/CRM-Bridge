d52ec936a54479165126fa854fe7bb93654096b1
Julien Mercier-Rojas <julien@jeckel-lab.fr>
2024-03-10 22:45:21 +0100

    Setup default and synced address books

--- src/Presentation/Controller/Setup/CardDavAccountController.php
+++ src/Presentation/Controller/Setup/CardDavAccountController.php
@@ -8,10 +8,14 @@ declare(strict_types=1);
 
 namespace App\Presentation\Controller\Setup;
 
+use App\Infrastructure\CardDav\AddressBookDiscovery;
 use App\Infrastructure\Doctrine\Entity\CardDavAccount;
+use App\Infrastructure\Doctrine\Entity\CardDavAddressBook;
+use App\Infrastructure\Doctrine\Repository\CardDavAddressBookRepository;
 use App\Infrastructure\Doctrine\Repository\CardDavConfigRepository;
-use App\Infrastructure\Doctrine\Repository\ImapAccountRepository;
 use App\Presentation\Form\CardDav\CardDavAccountFormType;
+use App\Presentation\Form\CardDav\DefaultAddressBookFormType;
+use Doctrine\ORM\EntityNotFoundException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -19,18 +23,25 @@ use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 use Symfony\Component\Translation\TranslatableMessage;
 
+use function App\new_uuid;
+
 #[Route("/setup/card_dav", name: "setup.card_dav.")]
 #[IsGranted("ROLE_ADMIN")]
 class CardDavAccountController extends AbstractController
 {
+    public function __construct(
+        private AddressBookDiscovery $addressBookDiscovery,
+        private CardDavAddressBookRepository $addressBookRepository,
+        private CardDavConfigRepository $cardDavConfigRepository
+    ) {}
+
     #[Route('/', name: 'index')]
-    public function index(
-        CardDavConfigRepository $cardDavConfigRepository
-    ): Response {
+    public function index(): Response
+    {
         return $this->render(
             'setup/embed/card_dav_index.html.twig',
             [
-                'card_dav_accounts' => $cardDavConfigRepository->findAll()
+                'card_dav_accounts' => $this->cardDavConfigRepository->findAll()
             ]
         );
     }
@@ -40,7 +51,7 @@ class CardDavAccountController extends AbstractController
         name: "create_account",
         methods: ['GET', 'POST']
     )]
-    public function addAccount(Request $request, CardDavConfigRepository $repository): Response
+    public function addAccount(Request $request): Response
     {
         $form = $this->createForm(
             CardDavAccountFormType::class,
@@ -53,7 +64,46 @@ class CardDavAccountController extends AbstractController
         if ($form->isSubmitted() && $form->isValid()) {
             /** @var CardDavAccount $account */
             $account = $form->getData();
-            $repository->persist($account);
+            $this->cardDavConfigRepository->persist($account);
+            $this->fetchAddressBookFromAccount($account);
+            return $this->redirect($this->generateUrl(
+                'setup.card_dav.setup_address_books',
+                ['accountId' => (string) $account->getId()]
+            ));
+        }
+        return $this->render(
+            'modal/form.html.twig',
+            [
+                'title' => 'card_dav.title.create_account',
+                'form' => $form->createView()
+            ]
+        );
+    }
+
+    #[Route(
+        path: "/account/{accountId}/setup_address_books",
+        name: "setup_address_books",
+        methods: ['GET', 'POST']
+    )]
+    public function setupAddressBooks(string $accountId, Request $request): Response
+    {
+        $account = $this->cardDavConfigRepository->getById($accountId);
+        $form = $this->createForm(
+            DefaultAddressBookFormType::class,
+            null,
+            [
+                'hx-post' => $this->generateUrl(
+                    'setup.card_dav.setup_address_books',
+                    ['accountId' => $accountId]
+                ),
+                'addressBooks' => $this->extractAddressBooksChoices($account)
+            ]
+        );
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            /** @var array{syncedAddressBooks: string[], defaultAddressBook: string} $data */
+            $data = $form->getData();
+            $this->updateAddressBooks($account, $data['syncedAddressBooks'], $data['defaultAddressBook']);
             return $this->render('modal/success.html.twig', [
                 'message' => new TranslatableMessage(
                     'setup.flash_message.card_dav_account_added',
@@ -69,9 +119,61 @@ class CardDavAccountController extends AbstractController
         return $this->render(
             'modal/form.html.twig',
             [
-                'title' => 'card_dav.title.create_account',
+                'title' => 'select address books',
                 'form' => $form->createView()
             ]
         );
+    }
+
+    private function fetchAddressBookFromAccount(CardDavAccount $account): void
+    {
+        foreach($this->addressBookDiscovery->discoverAddressBooks($account) as $addressBook) {
+            if (null !== $this->addressBookRepository->findOneBy([
+                    'uri' => $addressBook->getUri(),
+                    'cardDavAccount' => $account,
+                ])) {
+                continue;
+            }
+            $entity = (new CardDavAddressBook())
+//                ->setId(new_uuid())
+                ->setName($addressBook->getName())
+                ->setUri($addressBook->getUri())
+                ->setCardDavAccount($account);
+            $this->addressBookRepository->persist($entity);
+        }
+    }
+
+    /**
+     * @param CardDavAccount $account
+     * @return array<string, string>
+     */
+    private function extractAddressBooksChoices(CardDavAccount $account): array
+    {
+        $addressBookEntities = $this->addressBookRepository->findByAccount($account);
+        if (count($addressBookEntities) === 0) {
+            $this->fetchAddressBookFromAccount($account);
+            $addressBookEntities = $this->addressBookRepository->findByAccount($account);
+        }
+        $addressBooks = array_map(
+            static fn(CardDavAddressBook $addressBook): string => $addressBook->getName(),
+            $addressBookEntities
+        );
+        return array_combine($addressBooks, $addressBooks);
+    }
+
+    /**
+     * @param CardDavAccount $account
+     * @param string[] $syncedAddressBooks
+     * @param string $defaultAddressBook
+     * @return void
+     */
+    private function updateAddressBooks(CardDavAccount $account, array $syncedAddressBooks, string $defaultAddressBook): void
+    {
+        $addressBookEntities = $this->addressBookRepository->findByAccount($account);
+        foreach ($addressBookEntities as $addressBook) {
+            $addressBook->setEnabled(in_array($addressBook->getName(), $syncedAddressBooks, true));
+            $addressBook->setIsDefault($addressBook->getName() === $defaultAddressBook);
+            $this->addressBookRepository->persist($addressBook);
+        }
     }
 }
